d08197eb75f10622aaaf2e0394892949195aee6c
Fatemeh <f.khoshbayan@gmail.com>
2022-01-27 20:12:33 +0300

    Add new url to users urls for user management

--- awesome_website/users/urls.py
+++ awesome_website/users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard")
+    path("", views.dashboard, name="dashboard"),
+    path("^accounts/", include("django.contrib.auth.urls")),
 ]
