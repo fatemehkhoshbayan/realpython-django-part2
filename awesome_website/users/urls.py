bca4f480c231c21d1fe2d28b12fc4a67b49631b6
Fatemeh <f.khoshbayan@gmail.com>
2022-01-27 20:32:30 +0300

    Create login page

--- awesome_website/users/urls.py
+++ awesome_website/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("^accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
