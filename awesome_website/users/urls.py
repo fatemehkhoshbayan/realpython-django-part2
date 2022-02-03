aceaa7971c4296e47e1e372f856d181f10ff1c26
Fatemeh <f.khoshbayan@gmail.com>
2022-02-03 14:09:25 +0300

    Add github registration

--- awesome_website/users/urls.py
+++ awesome_website/users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
+    path("oauth/", include("social_django.urls")),
 ]
